dc3a9e627ad76bf1742ce0d4268a9ae3241bbd46
Matteo Balugani <supermatte.mb@gmail.com>
2021-06-24 18:20:38 +0200

    aggiunta vettorefile

--- src/VettoreCredenziali.java
+++ src/VettoreCredenziali.java
@@ -2,6 +2,7 @@ import java.util.Vector;
 
 public class VettoreCredenziali extends Vector<Credenziali> implements java.io.Serializable {
 
+
     public static final Credenziali CREDENZIALI_ADMIN = new Credenziali("kappa", "admin", "Via giovannina giornannina 44");
 
 }
--- src/VettoreOrdini.java
+++ src/VettoreOrdini.java
@@ -1,27 +1,13 @@
-import java.io.Serializable;
 import java.util.Vector;
 
-public class VettoreOrdini<E extends Serializable> implements java.io.Serializable {
-    private Vector listaordini;
+public class VettoreOrdini extends Vector<SpedizioneNormale> implements java.io.Serializable {
+
+    private static final String filename = "OrdiniClientiCriptati.txt";  //il file dove vengono salvati tutti gli ordini
+
 
     public VettoreOrdini() {
-        listaordini = new Vector(10);
-    }
 
-    public void aggiungi(E a) {
-        listaordini.add(a);
-    }
 
-    public void rimuovi(E p) {
-        listaordini.remove(p);
-    }
-
-    public E get(int index) {
-        return (E) listaordini.get(index);
-    }
-
-    public int size() {
-        return listaordini.size();
     }
 
 
